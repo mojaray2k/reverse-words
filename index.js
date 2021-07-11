388ced6940e8317c5e74f5ff8fbc928a5392f81f
Amen Moja Ra <mojaray2k@gmail.com>
2021-07-11 19:46:24 +0000

    Completing reverseWords algorithm

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 function reverseWords(string){
   // reverse every word in the string
   // return the new string
+
+  var wordsArr = string.split(' ');
+  var reversedWordsArr = [];
+
+  wordsArr.forEach(word => {
+    var reverseWord = '';
+    for(var i = word.length -1; i >= 0; i--) {
+      reverseWord += word[i];
+    }
+    reversedWordsArr.push(reverseWord);
+  })
+
+  return reversedWordsArr.join(' ');
 }
+
+reverseWords('this is a string of words');
+// => 'siht si a gnirts fo sdrow'
+
+reverseWords('Coding Javascript');
+// => 'gnidoC tpircsavaJ
+'
